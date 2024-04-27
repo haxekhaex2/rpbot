ac2b6ac2fd46402589f756d8a553b63abd53da34
haxekhaex2 <haxekhaex2@gmail.com>
2024-04-26 22:25:12 -0400

    improve directory generation

--- rpbot.py
+++ rpbot.py
@@ -1,4 +1,5 @@
 import discord
+import json
 import os
 
 class rpbot(discord.Client):
@@ -11,16 +12,23 @@ class rpbot(discord.Client):
 	async def on_ready(self):
 		print("%s is logged in!" % (client.user))
 
-	async def on_message(self, message):
-		if not message.guild.id in self.handlers:
-			self.handlers[message.guild.id] = server_handler(message.guild)
+	# Initialize an entry in handlers for the given server if one isn't already made.
+	async def check_handler(self, server):
+		if not server in self.handlers:
+			self.handlers[server] = server_handler(discord.utils.get(self.guilds, id = server))
+			await self.handlers[server].load_state()
+			
 
+	async def on_message(self, message):
+		await self.check_handler(message.guild.id)
 		await self.handlers[message.guild.id].process_message(message)
+		await self.handlers[message.guild.id].update_directory()
 
 class server_handler:
 	server = None
 	category = None
 	channel = None
+	directory = None
 
 	def __init__(self, server):
 		self.server = server
@@ -42,7 +50,7 @@ class server_handler:
 		if message.content.startswith("$category"):
 			argument = message.content.split(" ", 1)
 			if(len(argument) < 2):
-				await message.channel.send("no category specified!", delete_after = 5)
+				await message.channel.send("no argument specified!", delete_after = 5)
 				return
 			argument = argument[1]
 
@@ -57,7 +65,7 @@ class server_handler:
 		if message.content.startswith("$channel"):
 			argument = message.content.split(" ", 1)
 			if(len(argument) < 2):
-				await message.channel.send("no channel specified!", delete_after = 5)
+				await message.channel.send("no argument specified!", delete_after = 5)
 				return
 			argument = argument[1]
 
@@ -71,21 +79,41 @@ class server_handler:
 		# Command to print help.
 		if message.content.startswith("$help"):
 			info = str()
-			info += "here are my available commands:"
-			info += "$category - change the channel category from which to scrape posts"
-			info += "$channel - change the channel into which to repost messages and files"
+			info += "hello! i'm sillybot. i watch a given channel category and repost its messages into a given channel.\n" 
+			info += "here are my available commands:\n"
+			info += "$help - print this message to chat\n"
+			info += "$category - change the channel category from which to scrape posts\n"
+			info += "$channel - change the channel into which to repost messages and files\n"
+			info += "$directory - create a message wherein i hold a live directory of all channels and threads within the category\n"
+			info += "$save - save my state, including category, channel, and directory\n"
+			info += "$load - loads state, mostly for debugging\n"
 			info += "$dump - dump everything i know into chat"
-			info += "$help - print this message to chat"
 			await message.channel.send(info)
 			return
 
 		# Command to dump state to chat.
 		if message.content.startswith("$dump"):
 			info = str()
-			info += "server name: %s\n" % ("None" if self.server is None else self.server.name)
+			info += "server: %s\n" % ("None" if self.server is None else self.server.name)
 			info += "category: %s\n" % ("None" if self.category is None else self.category.name)
-			info += "channel: %s" % ("None" if self.channel is None else self.channel.name)
+			info += "channel: %s\n" % ("None" if self.channel is None else self.channel.jump_url)
+			info += "directory: %s" % ("None" if self.directory is None else self.directory.jump_url)
 			await message.channel.send(info)
+
+		# Create directory listing.
+		if message.content.startswith("$directory"):
+			self.directory = await message.channel.send("<3")
+			await 	self.update_directory()
+
+		# Save state.
+		if message.content.startswith("$save"):
+			await self.write_state()
+
+		# Load state.
+		if message.content.startswith("$load"):
+			if os.path.isfile(str(self.server.id) + ".json"):
+				with open(str(self.server.id) + ".json", "r", encoding = "utf-8") as file_handle:
+					await self.load_state(json.load(file_handle))
 
 		# Repost into the given channel if a message was sent anywhere in a category. 
 		if self.category is not None and self.channel is not None and message.channel.category == self.category:
@@ -95,6 +123,51 @@ class server_handler:
 
 			repost = await self.channel.send(content = "in %s:\n%s" % (message.jump_url, message.content), files = images)
 			await repost.edit(suppress = True)
+
+	async def update_directory(self):
+		info = str()
+
+		if self.category is None:
+			return None
+
+		# Iterate through text channels and their threads.
+		for channel in self.category.channels:
+			if not hasattr(channel, "threads") or len(channel.threads) < 1:
+				info += channel.jump_url + "\n"
+
+		info += "\n"
+		for channel in self.category.channels:
+			if hasattr(channel, "threads") and len(channel.threads) > 0:
+				info += channel.jump_url + "\n"
+				for thread in channel.threads:
+					info += "└───" + thread.jump_url + "\n"
+				info += "\n"
+
+		await self.directory.edit(content = info)	
+
+	async def write_state(self):
+		with open("data/" + str(self.server.id) + ".json", "w", encoding = "utf-8") as file_handle:
+			json.dump({
+				"category": None if self.category is None else self.category.id,
+				"channel": None if self.channel is None else self.channel.id,
+				"directory": {
+					"channel_id": None if self.directory is None else self.directory.channel.id,
+					"id":  None if self.directory is None else self.directory.id
+				}
+			}, file_handle);
+
+	async def load_state(self):
+		if os.path.isfile("data/" + str(self.server.id) + ".json"):
+			with open("data/" + str(self.server.id) + ".json", "r", encoding = "utf-8") as file_handle:
+				object = json.load(file_handle)
+				if object is None:
+					return
+
+		self.category = discord.utils.get(self.server.categories, id = object["category"])
+		self.channel = discord.utils.get(self.server.channels, id = object["channel"])
+		self.directory = discord.utils.get(self.server.channels, id = object["directory"]["channel_id"])
+		self.directory = None if self.directory is None else await self.directory.fetch_message(object["directory"]["id"])
+		return
 
 intents = discord.Intents.default()
 intents.message_content = True
